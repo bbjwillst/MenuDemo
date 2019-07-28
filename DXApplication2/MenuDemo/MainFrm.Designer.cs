0d07555cb284757163ec2f8b9d3d14d4e2b331b3
bbjwillst <bbjwillst@gmail.com>
2019-07-28 23:25:59 +0800

    Add project files.

---
+++ DXApplication2/MenuDemo/MainFrm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace MenuDemo
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraEditors.TileItemElement tileItemElement5 = new DevExpress.XtraEditors.TileItemElement();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.tileBar = new DevExpress.XtraBars.Navigation.TileBar();
+            this.tileBarGroupTables = new DevExpress.XtraBars.Navigation.TileBarGroup();
+            this.navigationFrame = new DevExpress.XtraBars.Navigation.NavigationFrame();
+            this.SetupNavigationPage = new DevExpress.XtraBars.Navigation.NavigationPage();
+            this.SetupTileBarItem = new DevExpress.XtraBars.Navigation.TileBarItem();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.navigationFrame)).BeginInit();
+            this.navigationFrame.SuspendLayout();
+            this.SetupNavigationPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tileBar
+            // 
+            this.tileBar.AllowGlyphSkinning = true;
+            this.tileBar.AllowSelectedItem = true;
+            this.tileBar.AppearanceGroupText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(140)))), ((int)(((byte)(140)))));
+            this.tileBar.AppearanceGroupText.Options.UseForeColor = true;
+            this.tileBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.tileBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tileBar.DropDownButtonWidth = 30;
+            this.tileBar.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            this.tileBar.Groups.Add(this.tileBarGroupTables);
+            this.tileBar.IndentBetweenGroups = 10;
+            this.tileBar.IndentBetweenItems = 10;
+            this.tileBar.ItemPadding = new System.Windows.Forms.Padding(8, 6, 12, 6);
+            this.tileBar.Location = new System.Drawing.Point(0, 0);
+            this.tileBar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tileBar.MaxId = 4;
+            this.tileBar.MaximumSize = new System.Drawing.Size(0, 118);
+            this.tileBar.MinimumSize = new System.Drawing.Size(117, 118);
+            this.tileBar.Name = "tileBar";
+            this.tileBar.Padding = new System.Windows.Forms.Padding(34, 12, 34, 12);
+            this.tileBar.ScrollMode = DevExpress.XtraEditors.TileControlScrollMode.None;
+            this.tileBar.SelectedItem = this.SetupTileBarItem;
+            this.tileBar.SelectionBorderWidth = 2;
+            this.tileBar.SelectionColorMode = DevExpress.XtraBars.Navigation.SelectionColorMode.UseItemBackColor;
+            this.tileBar.ShowGroupText = false;
+            this.tileBar.Size = new System.Drawing.Size(1037, 118);
+            this.tileBar.TabIndex = 1;
+            this.tileBar.Text = "tileBar";
+            this.tileBar.WideTileWidth = 150;
+            this.tileBar.SelectedItemChanged += new DevExpress.XtraEditors.TileItemClickEventHandler(this.tileBar_SelectedItemChanged);
+            // 
+            // tileBarGroupTables
+            // 
+            this.tileBarGroupTables.Items.Add(this.SetupTileBarItem);
+            this.tileBarGroupTables.Name = "tileBarGroupTables";
+            this.tileBarGroupTables.Text = "TABLES";
+            // 
+            // navigationFrame
+            // 
+            this.navigationFrame.Controls.Add(this.SetupNavigationPage);
+            this.navigationFrame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.navigationFrame.Location = new System.Drawing.Point(0, 118);
+            this.navigationFrame.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.navigationFrame.Name = "navigationFrame";
+            this.navigationFrame.Pages.AddRange(new DevExpress.XtraBars.Navigation.NavigationPageBase[] {
+            this.SetupNavigationPage});
+            this.navigationFrame.SelectedPage = this.SetupNavigationPage;
+            this.navigationFrame.Size = new System.Drawing.Size(1037, 540);
+            this.navigationFrame.TabIndex = 0;
+            this.navigationFrame.Text = "navigationFrame";
+            // 
+            // SetupNavigationPage
+            // 
+            this.SetupNavigationPage.Controls.Add(this.labelControl1);
+            this.SetupNavigationPage.Name = "SetupNavigationPage";
+            this.SetupNavigationPage.Size = new System.Drawing.Size(1037, 540);
+            // 
+            // SetupTileBarItem
+            // 
+            this.SetupTileBarItem.DropDownOptions.BeakColor = System.Drawing.Color.Empty;
+            tileItemElement5.ImageOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("resource.SvgImage")));
+            tileItemElement5.Text = "Setup";
+            this.SetupTileBarItem.Elements.Add(tileItemElement5);
+            this.SetupTileBarItem.Id = 3;
+            this.SetupTileBarItem.Name = "SetupTileBarItem";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(611, 235);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(33, 14);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Setup";
+            // 
+            // MainFrm
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 658);
+            this.Controls.Add(this.navigationFrame);
+            this.Controls.Add(this.tileBar);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.navigationFrame)).EndInit();
+            this.navigationFrame.ResumeLayout(false);
+            this.SetupNavigationPage.ResumeLayout(false);
+            this.SetupNavigationPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Navigation.TileBar tileBar;
+        private DevExpress.XtraBars.Navigation.NavigationFrame navigationFrame;
+        private DevExpress.XtraBars.Navigation.TileBarGroup tileBarGroupTables;
+        private DevExpress.XtraBars.Navigation.TileBarItem SetupTileBarItem;
+        private DevExpress.XtraBars.Navigation.NavigationPage SetupNavigationPage;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
---
+++ DXApplication2/MenuDemo/MainFrm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace MenuDemo
+{
+    public partial class MainFrm : DevExpress.XtraEditors.XtraForm
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+        private void tileBar_SelectedItemChanged(object sender, TileItemEventArgs e)
+        {
+            navigationFrame.SelectedPageIndex = tileBarGroupTables.Items.IndexOf(e.Item);
+        }
+    }
+}
